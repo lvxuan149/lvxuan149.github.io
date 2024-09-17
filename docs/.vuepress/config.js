c0ef2bd8e71a6be335fa4e9887cdb1f3137a131c
0xDragon888 <lvxuan147@gmail.com>
2024-09-18 02:18:52 +0800

    Update GitHub Actions workflow and configuration

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,8 +6,31 @@ module.exports = {
   themeConfig: {
     nav: [
       { text: '首页', link: '/' },
+      { text: '归档', link: '/archives/' },
+      { text: '分类', link: '/categories/' },
+      { text: '标签', link: '/tags/' },
       { text: '关于', link: '/about/' },
     ],
     sidebar: 'auto',
+    sidebarDepth: 2,
+    logo: '/img/logo.png', // 确保这个文件存在
+    author: {
+      name: '0xDragon888',
+      link: 'https://github.com/lvxuan149'
+    },
+    social: {
+      icons: [
+        { iconClass: 'icon-github', title: 'GitHub', link: 'https://github.com/lvxuan149' },
+        { iconClass: 'icon-twitter', title: 'Twitter', link: 'https://twitter.com/lvxuan147' },
+      ]
+    },
+    footer: {
+      createYear: 2024,
+      copyrightInfo: '0xDragon888 | MIT License'
+    },
   },
+  plugins: [
+    '@vuepress/back-to-top',
+    '@vuepress/medium-zoom',
+  ],
 }
